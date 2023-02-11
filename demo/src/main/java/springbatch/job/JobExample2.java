daceeb84c1f44dd5ed9806e7e7cac3b9be77d025
jhbaik1501 <jhbaik1501@gmail.com>
2023-02-12 01:47:57 +0900

    feat : JobExample2 생성 ! Flow 예시

--- demo/src/main/java/springbatch/job/JobExample2.java
+++ demo/src/main/java/springbatch/job/JobExample2.java
@@ -1,70 +1,111 @@
 package springbatch.job;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
 
 import lombok.RequiredArgsConstructor;
-import springbatch.service.ProductService;
 
-@Configuration
 @RequiredArgsConstructor
+@Configuration
 public class JobExample2 {
 
-	private final JobBuilderFactory jobBuilderFactory;
-	private final StepBuilderFactory stepBuilderFactory;
-	private final ProductService productService;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
-	@Bean
-	public Job make(ApplicationArguments args) throws Exception {
-		return jobBuilderFactory.get("productMakeJob")
-			.start(step1())
-			.next(step2())
-			// .incrementer(new RunIdIncrementer())
-			.build();
+    @Bean
+    public Job batchJob() {
+        StepDecider decider = new StepDecider();
 
-	}
+        return jobBuilderFactory.get("flowJob")
+                .incrementer(new RunIdIncrementer())
+                .start(flowJobStep1())
 
-	@Bean
-	public Step step1() {
-		Tasklet tasklet = (contribution, chunkContext) -> {
-			System.out.println("create data executed");
-			ExecutionContext stepExecutionContext = contribution.getStepExecution().getExecutionContext();
-			ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+                // .next(decider)
+                .on("COMPLETED").to(flowJobStep2())
+                // .from(decider)
+                .on("FAILED").to(flowJobStep3())
+                .end()
+                .build();
+    }
 
-			productService.addProductData(stepExecutionContext, jobExecutionContext);
-			return RepeatStatus.FINISHED;
-		};
+    @Bean
+    public Step flowJobStep1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 has executed");
+//                        throw new RuntimeException("");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
 
-		return stepBuilderFactory.get("makeStep")
-			.tasklet(tasklet)
-			.build();
-	}
+    @Bean
+    public Step flowJobStep2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
 
-	@Bean
-	public Step step2() {
-		return stepBuilderFactory.get("printStep")
-			.tasklet(new Tasklet() {
-				@Override
-				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+    @Bean
+    public Step flowJobStep3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step3 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
 
-					ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
-					System.out.println(jobExecutionContext.get("how much?") + "만큼의 실행이 끝났습니다!");
-					return null;
-				}
-			})
-			.build();
-	}
+    public class StepDecider implements JobExecutionDecider{
+        @Override
+        public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+            ExecutionContext jobExecutionContext = jobExecution.getExecutionContext();
+
+            Object ex = jobExecutionContext.get("ex");
+            if (ex == null) {
+                return FlowExecutionStatus.FAILED;
+            }
+            return FlowExecutionStatus.COMPLETED;
+        }
+    }
+
+
+    public class ExeuctionListener implements StepExecutionListener {
+        @Override
+        public void beforeStep(StepExecution stepExecution) {
+
+        }
+
+        @Override
+        public ExitStatus afterStep(StepExecution stepExecution) {
+            return ExitStatus.FAILED;
+        }
+    }
 }
---
+++ demo/src/main/java/springbatch/job/ProductMake.java
@@ -0,0 +1,68 @@
+package springbatch.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import springbatch.service.ProductService;
+
+@Configuration
+@RequiredArgsConstructor
+public class ProductMake {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+	private final ProductService productService;
+
+	@Bean
+	public Job make(ApplicationArguments args) throws Exception {
+		return jobBuilderFactory.get("productMakeJob")
+			.start(step1())
+			.next(step2())
+			// .incrementer(new RunIdIncrementer())
+			.build();
+
+	}
+
+	@Bean
+	public Step step1() {
+		Tasklet tasklet = (contribution, chunkContext) -> {
+			System.out.println("create data executed");
+			ExecutionContext stepExecutionContext = contribution.getStepExecution().getExecutionContext();
+			ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+
+			productService.addProductData(stepExecutionContext, jobExecutionContext);
+			return RepeatStatus.FINISHED;
+		};
+
+		return stepBuilderFactory.get("makeStep")
+			.tasklet(tasklet)
+			.build();
+	}
+
+	@Bean
+	public Step step2() {
+		return stepBuilderFactory.get("printStep")
+			.tasklet(new Tasklet() {
+				@Override
+				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+					ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+					System.out.println(jobExecutionContext.get("how much?") + "만큼의 실행이 끝났습니다!");
+					return null;
+				}
+			})
+			.build();
+	}
+}
